07c83f521e56d91cb888262a94d045ec500864c7
Victor Emmanuel <victor.emmanuel.8095@gmail.com>
2023-09-08 08:04:16 -0700

    First commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Print all the alphabet in lowercase in reverse using only putchar
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha
+	for (alpha = 'z'; alpha >= 'a'; c--)
+		putchar(alpha);
+	        putchar('\n');
+	return (0)
+}
