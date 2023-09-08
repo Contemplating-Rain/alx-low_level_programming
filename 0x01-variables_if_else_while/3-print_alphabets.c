8f22c79f47e0a0fb0f8eac9d96add3c1e9699b65
Victor Emmanuel <victor.emmanuel.8095@gmail.com>
2023-09-08 08:58:38 -0700

    Eigth commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-/* printing the alphabet */
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	char c;
@@ -9,7 +14,6 @@ int main(void)
 		putchar(c);
 	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
-	
 	putchar('\n');
 
 	return (0);
